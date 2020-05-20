6386247f658e9d67b7ed2ca4e511043b015f60b9
Davidson Fellipe <email@fellipe.com>
2020-05-20 12:52:57 -0400

    chore(ui): Improvements on mobile version

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,17 +1,23 @@
 import styled from 'styled-components';
 
 import { theme } from '../styles/theme';
+import { screen } from '../styles/screen';
 
 const Title = styled.h1`
   color: #333;
   font-style: normal;
   font-family: ${theme.light.fonts.title};
   font-weight: 900;
-  line-height: 65px;
-  margin: 75px 0 50px 0;
-  font-size: 60px;
-  letter-spacing: -2px;
+  line-height: 1em;
+  margin: 50px 0 25px 0;
+  font-size: 20px;
   text-transform: uppercase;
+
+  ${screen.lg} {
+    font-size: 60px;
+    letter-spacing: -2px;
+    margin: 50px 0 50px 0;
+  }
 `;
 
 export default Title;
